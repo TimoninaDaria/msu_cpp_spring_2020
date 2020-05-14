b47a16faf0bc07a101edbb01b6d605693c5c04e5
darik333 <darik93@yandex.ru>
2020-05-14 20:16:58 +0300

    HW

--- test/progress.cpp
+++ test/progress.cpp
@@ -1,23 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <deque>
+#include <vector>
+#include <condition_variable>
 #include <string>
 #include <iostream>
 #include <ctype.h>
 #include <vector>
-#include "pool.h"
 #include <sstream>
 #include <fstream>
 #include <future>
 #include <atomic>
 
-std::atomic_int sum_result = {0};
+#include "pool.h"
 
-void sumline(const std::string& line){
+int sumline(const std::string& line){
     int sum_result = 0;
     std::istringstream iss(line);
     int i = 0;
     while (iss >> i)
     {
-        ::sum_result += i;
+        sum_result += i;
     }
+    return sum_result;
 }
 
 int sum(const std::string file){
@@ -29,15 +38,16 @@ int sum(const std::string file){
     if(!myfile.is_open())
         throw std::runtime_error("Could not open file");
 
-    std::vector <std::future<void>> taskes;
+    std::vector <std::future<int>> taskes;
+    int sum_result {0};
     while(getline(myfile, line)){
         taskes.emplace_back(pool.exec(sumline, line));
     }
 
     for(auto &task: taskes)
-        task.wait();
+        sum_result += task.get();
 
-    return ::sum_result;
+    return sum_result;
 }
 
 int main(){
